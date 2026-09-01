919ee49bf11f16e7058c51871113cce32185321d
Axel Briggiler <axelbrig@gmail.com>
2026-09-01 10:59:28 -0300

    Funcion extraer terminada perteneciente a la clase ListaDobleEnlazada

--- biblioteca_ayed_fiuner/ayedfiuner/estructuras/ListaDobleEnlazada.py
+++ biblioteca_ayed_fiuner/ayedfiuner/estructuras/ListaDobleEnlazada.py
@@ -59,7 +59,53 @@ class ListaDobleEnlazada:
             self.tamanio += 1
             
     def extraer(self, posicion=None):
-        pass
+        '''Elimina y devuelve el item en la posicion especificada. O(1) para extraccion de los extremos'''
+
+        #Validacion
+        if self.esta_vacia():
+            raise IndexError ('La lista esta vacia.')
+        if posicion is None:
+            posicion=self._tamanio-1
+        if not isinstance (posicion,int):
+            raise TypeError ('La posicion del elemento debe ser un numero entero.')
+        if posicion<0 or posicion>=self._tamanio:
+            raise IndexError ('Posicion del elemento fuera de rango.')
+
+        #Extraemos primer elemento O(1)
+        if posicion ==0:
+            sacado=self._cabeza.dato
+            self._cabeza=self._cabeza.siguiente
+            if self._cabeza is not None:
+                self._cabeza.anterior=None
+            else:
+                self._cola=None 
+
+        #Extraemos el ultimo elemento O(1)
+        if posicion ==self.tamanio-1:
+            sacado=self._cola.dato
+            self._cola=self._cola.anterior
+            if self._cola is not None:
+                self._cola.siguiente=None
+            else:
+                self._cabeza=None
+
+        #Extraemos los elementos que se encuentran entre 1º y ultimo elemento O(N)
+        else:
+            if posicion < self.tamanio//2:
+                posicion_actual = self._cabeza
+                for _ in range (posicion):
+                    posicion_actual=posicion_actual.siguiente
+            else:
+                posicion_actual=self._cola
+                for _ in range(self._tamanio -1,posicion,-1):
+                    posicion_actual=posicion_actual.anterior
+
+            sacado=posicion_actual.dato
+            posicion_actual.anterior.siguiente=posicion_actual.siguiente
+            posicion_actual.siguiente.anterior=posicion_actual.anterior
+
+        self._tamanio=-1
+        return sacado
 
     def copiar(self):
         pass

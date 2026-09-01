fceaa5e7f8f1a2d5a0f08c904f45cb085d5638a6
Axel Briggiler <axelbrig@gmail.com>
2026-09-01 09:38:00 -0300

    elimina codigo repetido de la carpeta modulo1.py

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,9 +1,2 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
-class nodo:
-    '''La clase representa un nodo en una lista doblemente enlazada.'''
-    def ___init___(self, dato=None, anterior=None, siguiente=None):
-        self.dato=dato
-        self.anterior=anterior
-        self.siguiente=siguiente
-        

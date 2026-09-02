662807fd328153d47439a7293fccaf4cfd19e6ca
BautistaKadur <bautik2007@gmail.com>
2026-09-02 11:53:30 -0300

    Funciones copiar e invertir terminadas

--- biblioteca_ayed_fiuner/ayedfiuner/estructuras/ListaDobleEnlazada.py
+++ biblioteca_ayed_fiuner/ayedfiuner/estructuras/ListaDobleEnlazada.py
@@ -40,7 +40,7 @@ class ListaDobleEnlazada:
 
     def insertar(self, dato, posicion):
         if posicion < 0 or posicion > self.tamanio:
-            raise Exception("Posicion invalida")
+            raise IndexError("Posicion invalida")
         
         if posicion == 0:
           self.agregar_al_inicio(dato)
@@ -63,58 +63,73 @@ class ListaDobleEnlazada:
 
         #Validacion
         if self.esta_vacia():
-            raise IndexError ('La lista esta vacia.')
+            raise IndexError('La lista esta vacia.')
         if posicion is None:
-            posicion=self._tamanio-1
+            posicion = self.tamanio -1
         if not isinstance (posicion,int):
-            raise TypeError ('La posicion del elemento debe ser un numero entero.')
-        if posicion<0 or posicion>=self._tamanio:
-            raise IndexError ('Posicion del elemento fuera de rango.')
+            raise TypeError('La posicion del elemento debe ser un numero entero.')
+        if posicion < 0 or posicion >= self.tamanio:
+            raise IndexError('Posicion del elemento fuera de rango.')
 
         #Extraemos primer elemento O(1)
-        if posicion ==0:
-            sacado=self._cabeza.dato
-            self._cabeza=self._cabeza.siguiente
-            if self._cabeza is not None:
-                self._cabeza.anterior=None
+        if posicion == 0:
+            sacado = self.cabeza.dato
+            self.cabeza = self.cabeza.siguiente
+            if self.cabeza is not None:
+                self.cabeza.anterior = None
             else:
-                self._cola=None 
+                self.cola = None 
 
         #Extraemos el ultimo elemento O(1)
-        if posicion ==self.tamanio-1:
-            sacado=self._cola.dato
-            self._cola=self._cola.anterior
-            if self._cola is not None:
-                self._cola.siguiente=None
+        elif posicion == self.tamanio-1:
+            sacado = self.cola.dato
+            self.cola = self.cola.anterior
+            if self.cola is not None:
+                self.cola.siguiente = None
             else:
-                self._cabeza=None
+                self.cabeza = None
 
         #Extraemos los elementos que se encuentran entre 1º y ultimo elemento O(N)
         else:
-            if posicion < self.tamanio//2:
-                posicion_actual = self._cabeza
+            if posicion < self.tamanio // 2:
+                posicion_actual = self.cabeza
                 for _ in range (posicion):
-                    posicion_actual=posicion_actual.siguiente
+                    posicion_actual = posicion_actual.siguiente
             else:
-                posicion_actual=self._cola
-                for _ in range(self._tamanio -1,posicion,-1):
-                    posicion_actual=posicion_actual.anterior
+                posicion_actual=self.cola
+                for _ in range(self.tamanio -1, posicion, -1):
+                    posicion_actual = posicion_actual.anterior
 
-            sacado=posicion_actual.dato
-            posicion_actual.anterior.siguiente=posicion_actual.siguiente
-            posicion_actual.siguiente.anterior=posicion_actual.anterior
+            sacado = posicion_actual.dato
+            posicion_actual.anterior.siguiente = posicion_actual.siguiente
+            posicion_actual.siguiente.anterior = posicion_actual.anterior
 
-        self._tamanio=-1
+        self.tamanio -= 1
         return sacado
 
     def copiar(self):
-        pass
+        copia = ListaDobleEnlazada()
+        actual = self.cabeza
+        while actual is not None: #recorre la lista hasta encontrar None, O(N)
+            copia.agregar_al_final(actual.dato)
+            actual = actual.siguiente
+        return copia
 
     def invertir(self):
-        pass
+        actual = self.cabeza
+        self.cola = self.cabeza     #la cabeza actual se convierte en la cola
+        temp = None
+        while actual is not None:
+            #intercambia las referencias de anterior y siguiente de cada nodo
+            temp = actual.anterior
+            actual.anterior = actual.siguiente
+            actual.siguiente = temp
+            actual = actual.anterior    #accede al siguiente nodo 
+        if temp is not None:    #si la lista no estaba vacia le asigna la cabeza
+            self.cabeza = temp.anterior
 
     def concatenar(self, otra_lista):
-        pass        
+        pass
 
     def esta_vacia(self):
         pass
